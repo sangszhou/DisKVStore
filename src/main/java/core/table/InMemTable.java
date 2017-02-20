a7d4342324cf6aca4aeb42f5e5d22f96aa0afd81
sangszhou <xinshengchou@gmail.com>
2017-02-20 16:46:58 +0800

    remove inmemtable; ;

--- src/main/java/core/table/InMemTable.java
+++ src/main/java/core/table/InMemTable.java
@@ -1,36 +1,36 @@
-package core.table;
-
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by xinszhou on 2/17/17.
- */
-public class InMemTable implements Table {
-
-    String name;
-
-    public InMemTable(String name) {
-        this.name = name;
-    }
-
-    Map<String, String> data = new HashMap<>();
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public String get(String key) {
-        return data.get(key);
-    }
-
-    public String set(String key, String value) {
-        return data.put(key, value);
-    }
-
-    public int getSize() {
-        return data.size();
-    }
-}
+//package core.table;
+//
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * Created by xinszhou on 2/17/17.
+// */
+//public class InMemTable implements Table {
+//
+//    String name;
+//
+//    public InMemTable(String name) {
+//        this.name = name;
+//    }
+//
+//    Map<String, String> data = new HashMap<>();
+//
+//    @Override
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String get(String key) {
+//        return data.get(key);
+//    }
+//
+//    public String set(String key, String value) {
+//        return data.put(key, value);
+//    }
+//
+//    public int getSize() {
+//        return data.size();
+//    }
+//}
